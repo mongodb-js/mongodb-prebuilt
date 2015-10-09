61b057f6f979959f90135a1e0743b854a91abbd8
Roman Jurkov <winfinit@gmail.com>
2015-10-08 23:00:48 -0400

    added prep work for args

--- a.js
+++ a.js
@@ -1,5 +1,17 @@
 var mdb = require('./index');
 
-mdb.start_server({}, function(err) {
+mdb.start_server({
+	logs_callback: logs_callback, //optional
+	exit_callback: exit_callback, //optional
+}, function(err) {
     console.log(err);
 });
+
+function logs_callback(data) {
+	// do any type of parsing
+}
+
+function exit_callback(exit_code) {
+	// do something before terminating app
+	console.log('exit code: ', exit_code);
+}
--- index.js
+++ index.js
@@ -42,9 +42,22 @@ module.exports = {
 		if (!opts) {
 			opts = {};
 		}
+
 		var child = proc.spawn(this.bin_path(opts.version) + "mongod");
+		child.on('error', function (err) {
+		  debug('Failed to start child process.');
+		});
+		child.on('close', function (code) {
+			debug('child process exited with code ' + code);
+			if ( opts.exit_callback ) {
+				opts.exit_callback(code);
+			}
+		});
 		child.stderr.pipe(child.stdout);
 		child.stdout.on('data', function(data) {
+			if ( opts.logs_callback ) {
+				opts.logs_callback(data);
+			}
 			mongodb_logs(data.toString().slice(0, -1));
 		});
 	}
@@ -62,4 +75,29 @@ function dir_exists(dir) {
 		debug("error from lstat:", e);
 		return false;
 	}	
+}
+
+function build_mongod_cli(opts) {
+	var available_args = ["config", "port", "bind_ip", "ipv6", "maxConns", "logpath", 
+	"syslog", "syslogFacility", "logappend", "logRotate", "timeStampFormat",
+	"logappend", "logRotate", "timeStampFormat", "pidfilepath", "keyFile", "setParameter",
+	"httpinterface", "clusterAuthMode", "nounixsocket", "unixSocketPrefix", "filePermissions",
+	"fork", "auth", "noauth", "jsonp", "rest", "slowms", "profile", "cpu", "sysinfo", 
+	"noIndexBuildRetry", "noscripting", "notablescan", "oplogSize", "master", "slave",
+	"source", "only", "slavedelay", "autoresync", "replSet", "replIndexPrefetch", "configsvr",
+	"shardsvr", "storageEngine", "dbpath", "directoryperdb", "noprealloc", "nssize",
+	"quota", "quotaFiles", "smallfiles", "syncdelay", "upgrade", "repair", "repairpath",
+	"journal", "nojournal", "journalOptions", "journalCommitInterval", "wiredTigerCacheSizeGB",
+	"wiredTigerStatisticsLogDelaySecs", "wiredTigerJournalCompressor", 
+	"wiredTigerDirectoryForIndexes", "wiredTigerCollectionBlockCompressor",
+	"wiredTigerIndexPrefixCompression"];
+
+	var args_string = "";
+	available_args.forEach(function(mongo_key) {
+		if ( opts[mongo_key] ) {
+			var value = opts[mongo_key] === true ? "" : opts[mongo_key];
+			args_string += " --" + mongo_key + " " + value;
+		}
+	});
+	return args_string;
 }

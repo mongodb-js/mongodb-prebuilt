82ecad96e6d062c481f3326697760b066403254d
Roman Jurkov <winfinit@gmail.com>
2016-04-12 17:11:52 -0400

    formatting code

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ module.exports = {
     "active_version": active_version,
     "install": install,
     "start_server": start_server,
-	"shutdown": shutdown
+    "shutdown": shutdown
 };
 
 
@@ -23,10 +23,10 @@ module.exports = {
 var child_pid = 0;
 
 var shutdown = function(e) {
-	if(child_pid !== 0) {
-		debug('killing mongod process: %d', child_pid);
-		process.kill(child_pid);		
-	}
+    if (child_pid !== 0) {
+        debug('killing mongod process: %d', child_pid);
+        process.kill(child_pid);
+    }
 };
 
 process.on('exit', shutdown);
@@ -36,31 +36,31 @@ function start_server(opts, callback) {
         opts = {};
     }
 
-	if(! opts.args) {
-		opts.args = {};
-	}
+    if (!opts.args) {
+        opts.args = {};
+    }
 
-	if(opts.args.fork === undefined) {
-		opts.args.fork = true;
-	}
+    if (opts.args.fork === undefined) {
+        opts.args.fork = true;
+    }
 
-	if (! opts.args.logpath) {
-		var log_file = path.join(os.tmpdir(), 'mongodb-prebuilt-' + (new Date()).getTime() + '.log');
-		debug('logpath is', log_file);
-		opts.args.logpath = log_file;
-	}
+    if (!opts.args.logpath) {
+        var log_file = path.join(os.tmpdir(), 'mongodb-prebuilt-' + (new Date()).getTime() + '.log');
+        debug('logpath is', log_file);
+        opts.args.logpath = log_file;
+    }
     var args = build_args(opts);
 
     var bpath = bin_path(opts.version);
     if (!bpath) {
-		return install(opts.version, function(err) {
-			if(err) {
-				callback(err);
-			} else {
-            	bpath = bin_path(opts.version);
-            	return start();
-			}
-		});
+        return install(opts.version, function(err) {
+            if (err) {
+                callback(err);
+            } else {
+                bpath = bin_path(opts.version);
+                return start();
+            }
+        });
     } else {
         return start();
     }
@@ -68,33 +68,33 @@ function start_server(opts, callback) {
     function start() {
         debug("spawn", bpath + "mongod", args.join(' '));
         var child = proc.spawnSync(bpath + "mongod", args);
-		mongodb_logs(child.stdout.toString());
-		mongodb_logs(child.stderr.toString());
+        mongodb_logs(child.stdout.toString());
+        mongodb_logs(child.stderr.toString());
 
-		// error
-		if(child.status !== 0) {
+        // error
+        if (child.status !== 0) {
             if (opts.exit_callback) {
                 opts.exit_callback(child.status);
             }
-			callback(child.status);
-			return child.status;
-		}
+            callback(child.status);
+            return child.status;
+        }
 
-		// need to catch child pid
-		var child_pid_match = child.stdout.toString().match(/forked process:\s+(\d+)/i);
-		child_pid = child_pid_match[1];
+        // need to catch child pid
+        var child_pid_match = child.stdout.toString().match(/forked process:\s+(\d+)/i);
+        child_pid = child_pid_match[1];
 
-		// if mongod started, spawn killer
-		if (child.status === 0) {
-			debug('starting mongokiller.js, ppid:%d\tmongod pid:%d', process.pid, child_pid);
-        	var killer = proc.spawn("node", [path.join(__dirname, "binjs", "mongokiller.js"), process.pid, child_pid], {
-        	    stdio: 'ignore',
-				detached: true
-        	});
-        	killer.unref();
-		}
+        // if mongod started, spawn killer
+        if (child.status === 0) {
+            debug('starting mongokiller.js, ppid:%d\tmongod pid:%d', process.pid, child_pid);
+            var killer = proc.spawn("node", [path.join(__dirname, "binjs", "mongokiller.js"), process.pid, child_pid], {
+                stdio: 'ignore',
+                detached: true
+            });
+            killer.unref();
+        }
 
-		return child.status;
+        return child.status;
     }
 }
 
@@ -155,14 +155,14 @@ function install(version, callback) {
     if (dir_exists(bin_path)) {
         callback(false);
     } else {
-		var spawn_opts = [];
-		if (version) {
-			spawn_opts.push('--version', version);
-		}
-		if (process.env.https_proxy) {
-			spawn_opts.push('--https-proxy', process.env.https_proxy);	
-		}
-		var install_out = child_process.spawnFileSync('./install.js', spawn_opts);
+        var spawn_opts = [];
+        if (version) {
+            spawn_opts.push('--version', version);
+        }
+        if (process.env.https_proxy) {
+            spawn_opts.push('--https-proxy', process.env.https_proxy);
+        }
+        var install_out = child_process.spawnFileSync('./install.js', spawn_opts);
         callback(!!install_out.status);
     }
 }

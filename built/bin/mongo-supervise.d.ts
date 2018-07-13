09de412ee7799e93591d5ce5160e78bb9d6cda0c
Johannes Maas <johannes.maas@u-psud.fr>
2018-07-13 11:35:47 +0200

    Clean up root

--- built/bin/mongo-supervise.d.ts
+++
@@ -1,2 +0,0 @@
-declare let parentPid: number;
-declare let childPid: number;
--- built/mongodb-prebuilt-all.d.ts
+++
@@ -1,4 +0,0 @@
-export * from './mongo-bins';
-export * from './mongod-helper';
-export * from './mongodb-prebuilt';
-export * from './mongodb-supervise';
